08fd4e0d67c21f9edddac389fead22b0590555cb
Owl <66283153+maryjng94@users.noreply.github.com>
2021-10-26 21:18:03 -0400

    please push

--- script.js
+++ script.js
@@ -43,15 +43,17 @@ let shuffledColors = shuffle(COLORS);
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
+  gameContainer.setAttribute("data-flip", 0);
+
   for (let color of colorArray) {
     // create a new div
     const newDiv = document.createElement("div");
 
-    // give it a class attribute for the value we are looping over
-    newDiv.classList.add(flipped);
-    newDiv.classList.add(color);
-    newDiv.classList.toggle(color);
-    newDiv.setAttribute("data-color", color);;
+    // // give it a class attribute for the value we are looping over
+    // newDiv.classList.add(color);
+    // newDiv.classList.toggle(color);
+    newDiv.setAttribute("data-color", color);
+    newDiv.setAttribute("data-flip", "down");
     // newDiv.setAttribute("data-clicked", clicked);
 
     // call a function handleCardClick when a div is clicked on
@@ -62,48 +64,43 @@ function createDivsForColors(colorArray) {
   }
 }
 
-// count the number of cards that are flipped up, let flipped = 0
-// (flipped = 0; flipped < 2)
-    //listen for click to flip; flipped += 1
-// check if the two clicked cards match
-  // if not, unflip them
-// reset flipped to 0
-
-// flipped as the counter
-for (let flipped = 0; flipped < 2){
-  // listen for click, if a card has no flipped class it will toggle on and the counter goes up
-  gameDiv.addEventListener("click", function(e){
-      if (!(e.target.classList.contains(".flipped")){
-          e.target.classList.toggle("flipped")
-          flipped += 1
-      }
-  })
-}
-//Element.querySelectorAll('[data-attr="value"]')
-const cards = gameDiv.querySelectorAll
-// when flipped hits 2, compare the color class of the two cards with flipped class on.
-  // if it matches, flipped = 0
-
-// let cardOne = e.target
-// let cardTwo = e.target
-// if (cardOne.attribute("data-color") === cardTwo.attribute("data-color")){
-// }
-
-
-// a counter goes up when you click one then when you click another it hits 2 and disables all click event listeners for 1 second
-// if the data-color attribute matches then remove click event listeners on those divs
-// setTimeout( , 1000)
-// toggle the two card classes off
-
 
 // TODO: Implement this function!
 function handleCardClick(e) {
-  // you can use event.target to see which element was clicked
   assignedColor = e.target.getAttribute("data-color")
-  e.target.classList.toggle(assignedColor)
-  console.log("you just clicked", e.target);
-}
+  //assign the correct class based on data attribute
 
+  //check if there are 2 cards face up
+  //if not then flip the clicked one up and add one to the face up card count
+  if (e.target.parentElement.dataset.flip < 2){
+    e.target.classList.add(assignedColor);
+    if (e.target.dataset.flip === "down"){
+      e.target.dataset.flip = "up";
+      e.target.parentElement.dataset.flip += 1;
+      }
+    console.log("you just clicked", e.target);
+    }
+
+    //this doesn't work
+  if (e.target.parentElement.dataset.flip === 2){
+    const flippedCards = gameContainer.querySelectorAll("[data-flip = 'up']")
+    const arrFlippedCards = Array.from(flippedCards)
+    if(arrFlippedCards[0] === arrFlippedCards[1]){
+      for (let card in flippedCards){
+        card.removeEventListener("click", handleCardClick);
+      }
+    }
+    // else{
+    //   setTimeout(function(){
+    //     for (let card in flippedCards){
+    //       card.classList.remove(assignedColor)
+    //       }
+    //     }, 1000);
+    //   }
+    gameContainer.dataset.flip = 0
+
+  }
+}
 
 
 // when the DOM loads
